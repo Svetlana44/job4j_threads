3d3a855af5954887cd49431e734a81932db83995
SPB <shchaaas@mail.ru>
2024-01-01 21:57:46 +0300

    Thread.currentThread().getName() Thread#start() интерфейс java.lang.Runnable имеет один метод public void run().

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -16,4 +16,38 @@ public class ConcurrentOutput {
 }
 /* Метод Thread#start() указывает виртуальной машине,
  что операторы описанные в конструкторе нужно запустить в отдельной нити.
-Если убрать этот оператор, то вывода имени второй нити не будет. */
+Если убрать этот оператор, то вывода имени второй нити не будет.
+
+класс java.lang.Thread
+Конструктор этого класса принимает функциональный интерфейс java.lang.Runnable.
+Это интерфейс имеет один метод public void run().
+
+Чтобы не создавать анонимный класс, в примере выше использовалось лямбда-выражение.
+Thread another = new Thread(
+        () -> System.out.println(Thread.currentThread().getName())
+);
+Перепишем этот код через анонимный класс.
+Thread another = new Thread(
+        new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName())
+            }
+        }
+);
+Выглядит громоздко.
+Метод run имеет модификатор public.
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.run();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+В этом случае имя нити и в первом и во втором случае одинаковое.
+ Это происходит, потому что метод run не дает указания
+  выполнить свои операторы в отдельной нити, как это делаем метод Thread#start.
+   Метод run напрямую вызывает операторы в той же нити, в которой запущен этот метод.
+
+
+*/
