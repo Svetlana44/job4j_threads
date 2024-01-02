e3c0df8a9e419de060f482193ba9e7bcf8f38173
SpS <shchaaas@mail.ru>
2024-01-02 21:18:04 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+/*  Чтобы получить состояние нити можно воспользоваться методом - getState() */
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                }
+        );
+        Thread second = new Thread(() -> {
+        });
+        System.out.println(first.getName() + "  " + first.getState());
+        System.out.println(second.getName() + "  " + second.getState());
+        first.start();
+        second.start();
+
+        System.out.println(first.getName() + "  " + first.getState());
+        System.out.println(second.getName() + "  " + second.getState());
+/*        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + first.getState());
+        } */
+        while (!(first.getState() == Thread.State.TERMINATED
+                && second.getState() == Thread.State.TERMINATED)) {
+            System.out.println(first.getName() + "  " + first.getState());
+            System.out.println(second.getName() + "  " + second.getState());
+        }
+        System.out.println(first.getName() + "  " + first.getState());
+        System.out.println(second.getName() + "  " + second.getState());
+        System.out.println("Работа завершена.");
+
+    }
+}
