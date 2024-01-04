e3ab75474fb3c208222fb19125c858489d3dc854
SpS <shchaaas@mail.ru>
2024-01-04 22:05:57 +0300

    3. Состояние нити. [#229175 #470101] Замена && на ||

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -20,7 +20,7 @@ public class ThreadState {
             System.out.println(first.getName() + first.getState());
         } */
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getName() + "  " + first.getState());
             System.out.println(second.getName() + "  " + second.getState());
         }
