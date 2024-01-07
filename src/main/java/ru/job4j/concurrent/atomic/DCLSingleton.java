1a85dec65e03f8b309f092c087f0e24b412d0832
SpS <shchaaas@mail.ru>
2024-01-07 18:18:31 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/concurrent/atomic/DCLSingleton.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent.atomic;
+
+public class DCLSingleton {
+    private static volatile DCLSingleton instance;
+
+    public static DCLSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DCLSingleton.class) {
+                if (instance == null) {
+                    instance = new DCLSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private DCLSingleton() {
+    }
+}
